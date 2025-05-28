860e0bc7df13908e4a680cfd763fa92c18768ac6
alma <a@gamil.com>
2025-05-28 19:17:10 +0200

    css include oldalkeresnel

--- server/request.php
+++ server/request.php
@@ -19,7 +19,7 @@ class Request{
             
             switch (htmlspecialchars($_GET["oldal"])) {
                 case 'fooldal':
-                   // self::SetCssFajl("fooldal");
+                    self::SetCssFajl("fooldal");
                     Controller\FooldalController::main();
                     break;
                 case 'rolunk':
@@ -32,10 +32,10 @@ class Request{
         }
         return null;
     }
-  /* public static function SetCssFajl($fajl){ ?>
+    public static function SetCssFajl($fajl){ ?>
                 <link rel="stylesheet" href="./client/css/<?php echo $fajl;?>.css?v=1">
 
         <?php
-    }*/
+    }
 }
 ?>
