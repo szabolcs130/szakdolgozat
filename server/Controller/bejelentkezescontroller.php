273853755d347d459183b2d41830b54a915ab5d6
alma <a@gamil.com>
2025-06-03 16:38:18 +0200

    xss ellen kiiratas sajat adattal

--- server/Controller/bejelentkezescontroller.php
+++ server/Controller/bejelentkezescontroller.php
@@ -10,15 +10,15 @@ class BejelentkezesController{
         if (isset($_POST["username"]) && isset($_POST["p"])) {
             $username=$_POST["username"];
             $password=$_POST["p"];
-            echo $_POST["username"]."<br>";
-            echo $_POST["p"]."<br>";
+            //echo $_POST["username"]."<br>";
+            //echo $_POST["p"]."<br>";
             $user=BejelentkezesModel::GetSzemelyByName($username);
             if ($user && $user[0]['rang']==$password){//password_verify($user[0]['jelszo'],$password)) {
                 BejelentkezesView::SikeresBejelentkezes($user[0]["nev_szemely"]);
             }else{
                 BejelentkezesView::SikertelenBejelentkezes();
             }
-            var_dump($user);
+            //var_dump($user);
         }
     }
 }
