ca51ad462bd24994543b6e9de4129b1ec5396b77
alma <a@gamil.com>
2025-06-04 00:49:46 +0200

    alap fiok mvc mukodo

---
+++ server/View/fiokview.php
@@ -0,0 +1,17 @@
+<?php
+namespace Server\View;
+class FiokView{
+//
+    public static function ShowFiok($fiok){
+        if (is_array($fiok)) {
+            echo '<div id="fiokAdat">';
+            foreach ($fiok as $f) {
+                echo $f["nev_szemely"]." ".$f["email"];
+            }
+            echo '</div>';
+        }else{
+            return -1;
+        } 
+    }
+}
+?>
