c0a3cdceac9aabea47789f70f2bb6b81cb7f7264
alma <a@gamil.com>
2025-06-03 23:10:28 +0200

    css, session

--- server/View/bejelentkezesview.php
+++ server/View/bejelentkezesview.php
@@ -4,15 +4,17 @@ class BejelentkezesView{
 //
     public static function ShowBejelentkezes(){
       $html="";
-      $html.='<form method="post" action="?oldal=Bejelentkezes/auth">'.
-                '<input type="text" name="username" placeholder="Felhasznalonev"><br>'.
-                '<input type="password" name="p" placeholder="Jelszo"><br>'.
-                '<button type="submit">Bejelentkezes</button><br>'.
-             '</form>';
+      $html.= '<div id="bejelentkezesForm">'.
+                '<form method="post" action="?oldal=Bejelentkezes/auth">'.
+                  '<input type="text" name="username" placeholder="Felhasznalonev"><br><br>'.
+                  '<input type="password" name="p" placeholder="Jelszo"><br><br>'.
+                  '<button type="submit">Bejelentkezes</button>'.
+                '</form>'.
+              '</div>';
       echo $html; 
     }
     public static function SikeresBejelentkezes($nev){
-      echo "<h1>Sikeres bejelentkezes!".$nev."</h1>";
+      echo "<h1>Sikeres bejelentkezes, ".$nev."!</h1>";
     }
     public static function SikertelenBejelentkezes(){
       echo "<h1>Sikertelen bejelentkezes!</h1>";
