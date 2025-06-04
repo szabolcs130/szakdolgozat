181d9e2fb7467a49df62563a3d0b67fd8ad3b4c5
alma <a@gamil.com>
2025-06-04 02:29:04 +0200

    megfelelo jelszo ellenorzes

--- server/Controller/bejelentkezescontroller.php
+++ server/Controller/bejelentkezescontroller.php
@@ -11,7 +11,7 @@ class BejelentkezesController{
             $username=$_POST["username"];
             $password=$_POST["p"];
             $user=BejelentkezesModel::GetSzemelyByName($username);
-            if ($user && $user[0]['rang']==$password){//password_verify($user[0]['jelszo'],$password)) {
+            if ($user && password_verify($password,$user[0]['jelszo'])){
                 BejelentkezesView::SikeresBejelentkezes($user[0]["nev_szemely"]);
                 $_SESSION["username"]=$user[0]["nev_szemely"];
                 $_SESSION["rang"]=$user[0]["rang"];
