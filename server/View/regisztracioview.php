00db4eaa70818b744e57e97af21d539d06b731b2
alma <a@gamil.com>
2025-06-04 02:06:29 +0200

    alap regisztracio mvc mukodo

---
+++ server/View/regisztracioview.php
@@ -0,0 +1,24 @@
+<?php
+namespace Server\View;
+class RegisztracioView{
+//
+    public static function ShowRegisztracio(){
+      $html="";
+      $html.= '<div id="regisztracioForm">'.
+                '<form method="post" action="?oldal=Regisztracio/log">'.
+                  '<input type="text" name="username" placeholder="Felhasznalonev"><br><br>'.
+                  '<input type="text" name="email" placeholder="Email"><br><br>'.
+                  '<input type="password" name="p" placeholder="Jelszo"><br><br>'.
+                  '<button type="submit">Regisztracio</button>'.
+                '</form>'.
+              '</div>';
+      echo $html; 
+    }
+    public static function SikeresRegisztracio(){
+      echo "<h1>Sikeres regisztracio!</h1>";
+    }
+    public static function SikertelenRegisztracio(){
+      echo "<h1>Sikertelen regisztracio!</h1>";
+    }
+}
+?>
