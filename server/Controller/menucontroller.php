76c264338e248ce340323e6f6abc6a1a8202cf96
alma <a@gamil.com>
2025-06-14 18:43:30 +0200

    visszateresi ertekek javit, potol menu,requestfajl

--- server/Controller/menucontroller.php
+++ server/Controller/menucontroller.php
@@ -12,13 +12,12 @@ class MenuController{
         }else{
             $menu=MenuModel::GetMenuByRang(0);
         }
-        if ($menu!=-1) {
-            if (MenuView::ShowMenu($menu)==-1) {
-                return -1;
+        if ($menu) {
+            if (MenuView::ShowMenu($menu)) {
+                return 1;
             }
-        }else{
-            return -1;
         }
+            return 0;
     }
 }
 ?>
