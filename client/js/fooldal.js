dca129f25536d76d5f064ada52a4d31812bf8e46
alma <a@gamil.com>
2025-11-04 15:41:28 +0100

    api termekek fajl es jsben meghivasa javitas

--- client/js/fooldal.js
+++ client/js/fooldal.js
@@ -1,7 +1,7 @@
 //import { Listazo } from './aruListazo.js';
 
 window.onload = async function() {
-    let response= await fetch("?oldal=ApiTermekek/Main");
+    let response= await fetch("?oldal=Apitermekek/Main");
     let data = await response.json();
     //Listazo(data);
     //console.log(data);
