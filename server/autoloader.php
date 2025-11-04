c8d70f6f2159608b9f356f5e2fe6327225dbe4fe
alma <a@gamil.com>
2025-11-04 15:28:14 +0100

    autoloader javit

--- server/autoloader.php
+++ server/autoloader.php
@@ -3,7 +3,7 @@ namespace Server;
 class Autoloader{
 public static function Main(){
         spl_autoload_register(function ($osztaly){
-            $osztaly=strtolower($osztaly);
+            //$osztaly=strtolower($osztaly);
             $osztaly=str_replace("\\","/",$osztaly);
             if (file_exists($osztaly.".php")) {
                 require_once($osztaly.".php");
