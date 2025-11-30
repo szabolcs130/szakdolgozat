29c3ae508dcb84c11e865641282926fd841fdff8
alma <a@gamil.com>
2025-11-30 20:31:10 +0100

    keresok javit ha nincs talalat majd vissza

--- client/js/fooldal.js
+++ client/js/fooldal.js
@@ -14,51 +14,60 @@ window.onload = async function() {
 async function Lapoz(data) {
     const aruk=document.getElementById('aruk');
     aruk.innerHTML="";
-    data.forEach(element => {
+    if (data.length) {
+        document.getElementById("lapozo").style.display="flex";
+        data.forEach(element => {
 
-        const aru=document.createElement('div');
-        aru.classList.add('aru');
+            const aru=document.createElement('div');
+            aru.classList.add('aru');
 
-        const aru_nev=document.createElement('div');
-        aru_nev.classList.add('aru_nev');
-        aru_nev.textContent=element.nev_aru;
+            const aru_nev=document.createElement('div');
+            aru_nev.classList.add('aru_nev');
+            aru_nev.textContent=element.nev_aru;
 
-        const aru_kep=document.createElement('img');
-        aru_kep.classList.add('aru_kep');
-        const kepUrl=new URL('../image/'+element.kep+'.png', import.meta.url).href;
-        aru_kep.src=kepUrl;
-        aru_kep.alt="Nem sikerult a kep betoltes!";
+            const aru_kep=document.createElement('img');
+            aru_kep.classList.add('aru_kep');
+            const kepUrl=new URL('../image/'+element.kep+'.png', import.meta.url).href;
+            aru_kep.src=kepUrl;
+            aru_kep.alt="Nem sikerult a kep betoltes!";
 
-        const aru_ar=document.createElement('div');
-        aru_ar.classList.add('aru_ar');
-        aru_ar.textContent=element.ar+" Forint";
+            const aru_ar=document.createElement('div');
+            aru_ar.classList.add('aru_ar');
+            aru_ar.textContent=element.ar+" Forint";
 
-        const aru_mennyiseg=document.createElement('div');
-        aru_mennyiseg.classList.add('aru_mennyiseg');
-        aru_mennyiseg.textContent=element.mennyiseg ? "Raktáron: "+element.mennyiseg+"db" : "Elfogyott";
+            const aru_mennyiseg=document.createElement('div');
+            aru_mennyiseg.classList.add('aru_mennyiseg');
+            aru_mennyiseg.textContent=element.mennyiseg ? "Raktáron: "+element.mennyiseg+"db" : "Elfogyott";
 
-        const aru_leiras=document.createElement('div');
-        aru_leiras.classList.add('aru_leiras');
-        aru_leiras.textContent=element.leiras;
-        
-        const liTag=document.createElement('li');
+            const aru_leiras=document.createElement('div');
+            aru_leiras.classList.add('aru_leiras');
+            aru_leiras.textContent=element.leiras;
+            
+            const liTag=document.createElement('li');
 
-        const aTag=document.createElement('a');
-        aTag.href='?oldal=Termek/Main/'+element.id_aru;
-        aTag.textContent="Megnéz";
+            const aTag=document.createElement('a');
+            aTag.href='?oldal=Termek/Main/'+element.id_aru;
+            aTag.textContent="Megnéz";
 
-        liTag.appendChild(aTag);
-        
-        aru.appendChild(aru_nev);
-        aru.appendChild(aru_kep);
-        aru.appendChild(aru_ar);
-        aru.appendChild(aru_leiras);
-        aru.appendChild(aru_mennyiseg);
-        aru.appendChild(liTag);
+            liTag.appendChild(aTag);
+            
+            aru.appendChild(aru_nev);
+            aru.appendChild(aru_kep);
+            aru.appendChild(aru_ar);
+            aru.appendChild(aru_leiras);
+            aru.appendChild(aru_mennyiseg);
+            aru.appendChild(liTag);
 
-        aruk.appendChild(aru);
+            aruk.appendChild(aru);
 
-    });
+        });
+    }else{
+        const keresesEredmenyNincsh3=document.createElement("h3");
+        keresesEredmenyNincsh3.textContent="Nincs talalat! ";
+        aruk.appendChild(keresesEredmenyNincsh3);
+        document.getElementById("lapozo").style.display="none";
+    }
+    
     window.scrollTo(0,0);
     ElozoKovetkezoLapozoMegjelenitese();
 }
