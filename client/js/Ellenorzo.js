bd9c2e65166113a05185f6e137a129c32ca85a0e
alma <a@gamil.com>
2025-12-01 20:14:21 +0100

    fooldal szuro js validacio szovegnel

---
+++ client/js/Ellenorzo.js
@@ -0,0 +1,69 @@
+export function EllenorizElsoResz(keresoGomb,error,adat,lehetUres,minta,min,max) {
+    if (keresoGomb) {
+        keresoGomb.disabled=true;
+    }
+    if (lehetUres){
+        if (adat.value.length==0) {
+            keresoGomb.disabled=false;
+            adat.style.background='#9f9';
+            error.style.display='none';
+            error.textContent="";
+        }else{
+            const eredmeny=szovegEllenorzo(error,adat,min,max,minta,lehetUres);
+            if (eredmeny) {
+                keresoGomb.disabled=!eredmeny;
+            }
+        }
+    }else{
+       const eredmeny=szovegEllenorzo(error,adat,min,max,minta,lehetUres);
+        if (eredmeny) {
+            keresoGomb.disabled=!eredmeny;
+        }
+    }
+}
+export function szovegEllenorzo(error,adat,min,max,minta,lehetUres) {
+    var rendben=true;
+    //const b=adat.value;
+    error.textContent="";
+    error.style.color="red";
+    error.textContent="";
+    error.style.display="none";
+    if (!minta.test(adat.value)){
+        error.textContent="Tartsa be a mintat!";
+        rendben=false;
+        adat.focus();
+        adat.style.background='#f99';
+        error.style.display="inline-block";
+        return rendben;
+    }
+    if (!adat.value.trim() || (adat.value.length>0 && adat.value[0]==" ")) {
+        error.textContent="Szokoz nem megengedett elol!";
+        rendben=false;
+        adat.focus();
+        adat.style.background='#f99';
+        error.style.display="inline-block";
+        return rendben;
+    }
+    if (adat.value.length==0 && lehetUres==false) {
+        error.textContent="Ures!";
+        rendben=false;
+        adat.focus();
+        adat.style.background='#f99';
+        error.style.display="inline-block";
+        return rendben;
+    }
+    if (adat.value.length<min || adat.value.length>max) {
+        error.textContent="Hossz nem megfelelo";
+        rendben=false;
+        adat.focus();
+        adat.style.background='#f99';
+        error.style.display="inline-block";
+        return rendben;
+    }
+    if (rendben) {
+        adat.style.background='#9f9';
+        error.style.display='none';
+        error.textContent="";
+    }
+    return rendben;
+}
--- client/js/Lapozo.js
+++ client/js/Lapozo.js
@@ -1,3 +1,4 @@
+import { EllenorizElsoResz,szovegEllenorzo } from './Ellenorzo.js';;
 export async function SzuroFelepit(param,maximumAr,callback) {
     const szuroTarolo=document.getElementById("szuroTarolo");
     const minArLabel=document.createElement("label");
@@ -33,15 +34,27 @@ export async function SzuroFelepit(param,maximumAr,callback) {
     minArDiv.id="minArDiv";
     minArDiv.appendChild(minArLabel);
     minArDiv.appendChild(minArInput);
-
+    
     let nevKeresInput=document.createElement("input");
     nevKeresInput.type="text";
-    nevKeresInput.placeholder="Kulcsszo"
-    nevKeresInput.id="nevKeres"
+    nevKeresInput.placeholder="Kulcsszo";
+    nevKeresInput.id="nevKeres";
+    nevKeresInput.minLength=0;
+    nevKeresInput.maxLength=10;
+    nevKeresInput.addEventListener("input",(e)=>{
+        EllenorizElsoResz(szuresBekuldGomb,errorP,nevKeresInput,true,/^[A-Za-z ]+$/,0,3);
+    });
+
+    const errorP=document.createElement("p");
+    errorP.style.display="none";
+    const nevErrorDiv=document.createElement("div");
+    nevErrorDiv.id="nevErrorDiv";
+    nevErrorDiv.appendChild(nevKeresInput);
+    nevErrorDiv.appendChild(errorP);
+
     let szuresBekuldGomb=document.createElement('button');
     szuresBekuldGomb.type="submit";
     szuresBekuldGomb.textContent="Keres";
-
 
     minArInput.addEventListener("input",(e)=>{
         minArLabel.textContent="Min: "+e.target.value;
@@ -72,7 +85,9 @@ export async function SzuroFelepit(param,maximumAr,callback) {
             minArInput.disabled=false;
         }
     });
-    szuroTarolo.appendChild(nevKeresInput);
+    //szuroTarolo.appendChild(nevKeresInput);
+    //szuroTarolo.appendChild(errorP);
+    szuroTarolo.appendChild(nevErrorDiv);
     szuroTarolo.appendChild(minArDiv);
     szuroTarolo.appendChild(maxArDiv);
     szuroTarolo.appendChild(szuresBekuldGomb);
