80b73c81e8b8c0aea557630c6b6ab297be735c24
alma <a@gamil.com>
2025-12-06 16:29:33 +0100

    regisztraciocontroller validacio

--- server/Controller/regisztraciocontroller.php
+++ server/Controller/regisztraciocontroller.php
@@ -2,6 +2,7 @@
 namespace Server\Controller;
 use Server\View\RegisztracioView;
 use Server\Model\BejelentkezesModel;
+use Server\Model\ErtekEllenorzesModel;
 class RegisztracioController{
     public static function Main(){
         RegisztracioView::ShowRegisztracio();
@@ -9,13 +10,21 @@ class RegisztracioController{
     }
     public static function EllenorizRegisztracio(){
         if (!isset($_SESSION["username"]) && isset($_POST["username"]) && isset($_POST["p"]) && isset($_POST["email"])) {//!isset($_SESSION["username"]) && 
-            $username=$_POST["username"];
-            $email=$_POST["email"];
-            $password=$_POST["p"];
+            $username=ErtekEllenorzesModel::Szoveg($_POST["username"],5,20,"/^[A-Za-z0-9]+$/");
+            if ($username===false) {
+                return 0;
+            }
+            $email=ErtekEllenorzesModel::Szoveg($_POST["email"],1,254,"/^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,3}$/");
+            if ($email===false) {
+                return 0;
+            }
+            $password=ErtekEllenorzesModel::Szoveg($_POST["p"],12,64,"/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{12,64}$/");
+            if ($password===false) {
+                return 0;
+            }
             $user=BejelentkezesModel::GetSzemelyByEmail($email);
-            if ($user!=0 && !$user){
+            if (is_array($user) && empty($user)){//$user!=0 && !$user
                 if (BejelentkezesModel::hozzaadSzemely($username,$email,$password,1)!=0) {
-                    //RegisztracioView::SikeresRegisztracio();
                     $_SESSION["uzenet"]="Sikeres regisztracio!";
                     header('Location: ?oldal=Bejelentkezes');
                     exit();
