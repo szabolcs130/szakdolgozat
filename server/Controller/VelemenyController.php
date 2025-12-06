ed01a173acccbe009d9913fb33f1968362dbfd35
alma <a@gamil.com>
2025-12-06 18:13:58 +0100

    VelemenyController validalas

--- server/Controller/VelemenyController.php
+++ server/Controller/VelemenyController.php
@@ -2,37 +2,45 @@
 namespace Server\Controller;
 use Server\View\VelemenyView;
 use Server\Model\VelemenyModel;
+use Server\Model\ErtekEllenorzesModel;
 class VelemenyController{
     public static function VelemenyBekuld(){
         if (isset($_SESSION["username"]) && isset($_POST["velemenyInput"]) && isset($_POST["aruId"])) {
-            if (is_numeric($_POST["aruId"])){
-                $velemenyVanE=VelemenyModel::lekerdezVelemenyByAruIdSajat($_POST["aruId"],$_SESSION['userId']);
-                if (empty($velemenyVanE) && VelemenyModel::hozzaadVelemeny($_SESSION["userId"],$_POST["aruId"],$_POST["velemenyInput"])!=0) {
-                    if (isset($_SESSION['teljeskeres'])) {
-                        header('Location: '.$_SESSION['teljeskeres']);
-                        exit();
-                    }
-                    header('Location: ?oldal=Fooldal');
+            $id=ErtekEllenorzesModel::Szam($_POST["aruId"],0,99999999);
+            if ($id===false) {
+                return 0;
+            }
+            $velemeny=ErtekEllenorzesModel::Szoveg($_POST["velemenyInput"],5,254,"/^[A-Za-z0-9áéíóöőúüűÁÉÍÓÖŐÚÜŰ .,!?;:]+$/");
+            if ($velemeny===false) {
+                return 0;
+            }
+            $velemenyVanE=VelemenyModel::lekerdezVelemenyByAruIdSajat($id,$_SESSION['userId']);
+            if (is_array($velemenyVanE) && empty($velemenyVanE) && VelemenyModel::hozzaadVelemeny($_SESSION["userId"],$id,$velemeny)!=0) {
+                if (isset($_SESSION['teljeskeres'])) {
+                    header('Location: '.$_SESSION['teljeskeres']);
                     exit();
-                } 
+                }
+                header('Location: ?oldal=Fooldal');
+                exit();
             }
         }
         return 0;
     }
     public static function VelemenyTorles(){
         if (isset($_SESSION["username"]) && isset($_POST["idVelemeny"])) {
-            $vId=$_POST["idVelemeny"];
+            $vId=ErtekEllenorzesModel::Szam($_POST["idVelemeny"],0,99999999);
+            if ($vId===false) {
+                return 0;
+            }
             $aruId=$_SESSION['parameter'] ?? null;
             $userId=$_SESSION['userId'] ?? null;
-            if (is_numeric($vId) && $aruId!=null && $aruId!=null) {
-                if (VelemenyModel::VelemenyTorol($aruId,$userId,$vId)!=0) {
-                    if (isset($_SESSION['teljeskeres'])) {
-                        header('Location: '.$_SESSION['teljeskeres']);
-                        exit();
-                    }
-                    header('Location: ?oldal=Fooldal');
+            if (VelemenyModel::VelemenyTorol($aruId,$userId,$vId)!=0) {
+                if (isset($_SESSION['teljeskeres'])) {
+                    header('Location: '.$_SESSION['teljeskeres']);
                     exit();
                 }
+                header('Location: ?oldal=Fooldal');
+                exit();
             }
         }
         return 0;
